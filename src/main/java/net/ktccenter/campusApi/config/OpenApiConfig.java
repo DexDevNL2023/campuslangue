6515bdca8db33e14d29b79100438c51b66798e29
DexDevNL2023 <dexternlang@gmail.com>
2024-01-31 09:05:57 +0100

    Correct cors method 2

--- src/main/java/net/ktccenter/campusApi/config/OpenApiConfig.java
+++ src/main/java/net/ktccenter/campusApi/config/OpenApiConfig.java
@@ -1,15 +1,10 @@
 package net.ktccenter.campusApi.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-    @Bean
+    /*@Bean
     public OpenAPI openAPI() {
         final String securitySchemeName = "Authorization";
         return new OpenAPI()
@@ -24,5 +19,5 @@ public class OpenApiConfig {
                                                 .bearerFormat("JWT")
                                 )
                 );
-    }
+    }*/
 }
