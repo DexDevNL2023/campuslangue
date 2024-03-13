85a5d41f0e35dd9bdf436a43f46d1e211f038fda
DexDevNL2023 <dexternlang@gmail.com>
2024-03-13 14:19:06 +0100

    update accès 2

--- src/main/java/net/ktccenter/campusApi/validators/AuthorizeUser.java
+++ src/main/java/net/ktccenter/campusApi/validators/AuthorizeUser.java
@@ -1,12 +1,16 @@
 package net.ktccenter.campusApi.validators;
 
+import javax.validation.Constraint;
+import javax.validation.ReportAsSingleViolation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Constraint(validatedBy = AuthorizeUserValidatorConstraint.class)
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@ReportAsSingleViolation
 public @interface AuthorizeUser {
     String actionKey();
 }
